b65ef131595c0a30e7de13b11a9fd01e95046ea0
Rafa123F <rm.af@hotmail.com>
2024-10-09 17:57:56 +0400

    forth  commit

--- js/app.js
+++ js/app.js
@@ -25,3 +25,43 @@ console.log(myNav);
 
 //map it to myMenu
 myMenu.appendChild(myNav);
+
+
+
+// create some cards in the footer
+const addfoot= document.querySelector('.foot');
+
+//create an array contain the details of the cards
+const myCards = [
+    {
+        detail1: 'Home',
+        detail2: 'Apout Us',
+        detail3: 'Contact Us'
+    },
+    {
+        detail1: 'Follow Us',
+        detail2: 'Instgram',
+        detail3: 'Facebook'
+    },
+    {
+        detail1: 'My blog website',
+        detail2: 'Galary',
+        detail3: 'Copyright 2024' 
+    }
+];
+
+//create a function that map the cards array to an elment in html 
+function creatCards(card) {
+
+    const myh3 = document.createElement('h3');
+    myh3.classList.add("box");
+    myh3.innerHTML=`
+    ${card.detail1}</br>
+    ${card.detail2} </br>
+    ${card.detail3}
+    `
+    addfoot.appendChild(myh3);
+}
+//use forEach loop to print out the function result for each cards in html
+myCards.forEach(creatCards);
+
